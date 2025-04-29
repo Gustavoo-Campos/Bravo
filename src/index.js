c90e4f587ef9225494ae2f60ab73921bc4ca1f6c
Gustavo Campo de Souza <160344662+Gustavoo-Campos@users.noreply.github.com>
2025-04-29 17:52:10 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 
+
+const toggleTheme = document.getElementById('theme');
+
+const rootHtml = document.documentElement; 
+
+
+
+function changeTheme(){
+    /*toggleTheme.classList.toggle('dark-theme');*/
+
+   const currentTheme = rootHtml.getAttribute('data-theme');
+
+if (currentTheme === 'dark') {
+    rootHtml.setAttribute('data-theme', 'light');
+   toggleTheme.innerHTML = '<img src="../img/sol.svg" alt="Light Theme">';
+
+   
+
+}   else {
+    rootHtml.setAttribute('data-theme', 'dark');
+    toggleTheme.innerHTML = '<img src="../img/moon.svg" alt="Dark Theme">';
+  
+
+}
+}
+
+toggleTheme.addEventListener('click', changeTheme);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
